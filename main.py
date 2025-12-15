11414d1df100b89cf9c842996671a56a89d849dc
Leon Seibert <sl6363@mci4me.at>
2025-12-15 21:40:18 +0100

    Mockup der Geräte-Verwaltung angefangen

--- main.py
+++ main.py
@@ -1,27 +1,2 @@
-import streamlit as st
+import ui_device
 
-# Eine Überschrift der ersten Ebene
-st.write("# Gerätemanagement")
-
-# Eine Überschrift der zweiten Ebene
-st.write("## Geräteauswahl")
-
-# Eine Auswahlbox mit hard-gecoded Optionen, das Ergebnis
-
-current_device = st.selectbox(label='Gerät auswählen',
-        options = ["Gerät_A", "Gerät_B"])
-
-st.write(f"Das ausgewählte Gerät ist {current_device}")
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
-
-with tab1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-
-with tab2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-with tab3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
